45a69c51b572a39633277505236d0de5d9f1e9a9
Muhammad Moaz <m-moaz@hotmail.com>
2016-02-10 19:53:34 +0300

    Added launch screen and code improvements

--- HelloOBJC/ViewController.m
+++ HelloOBJC/ViewController.m
@@ -17,44 +17,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     _askAQuestion.layer.cornerRadius = 6.0;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) checkAnswer:(NSString*) givenAnswer {
+    
+    NSString *title = [[NSString alloc] init];
+    NSString *message = [[NSString alloc] init];
+    NSString *buttonText = [[NSString alloc] init];
+    
+    if ([givenAnswer isEqualToString: @"Yes"]) {
+        title = @"That's Right!";
+        message = @"You are super cool too.";
+        buttonText = @"Thanks";
+        
+    } else {
+        title = @"What a bummer!";
+        message = @"Neither are you.";
+        buttonText = @"Okay :(";
+    }
+    
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:title
+                                message:message
+                                preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction
+                                   actionWithTitle:buttonText
+                                   style:UIAlertActionStyleCancel
+                                   handler:nil];
+    
+    [alert addAction:cancelAction];
+    [self presentViewController:alert animated:YES completion:nil];
+    
+    
 }
 
 - (IBAction)askAQuestionButton:(UIButton *)sender {
     
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"What do you think?" message:@"Is Moaz super cool?" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:@"What do you think?"
+                                message:@"Is Moaz super cool?"
+                                preferredStyle:UIAlertControllerStyleAlert];
     
-    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"That's Right!" message:@"You are super cool too." preferredStyle:UIAlertControllerStyleAlert];
-        
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Thanks" style:UIAlertActionStyleCancel handler:nil];
-        [alert addAction:cancelAction];
-        [self presentViewController:alert animated:YES completion:nil];
-        
-    }];
+    UIAlertAction *yesAction = [UIAlertAction
+                                actionWithTitle:@"Yes"
+                                style:UIAlertActionStyleDefault
+                                handler:^(UIAlertAction * _Nonnull action) {
+                                    [self checkAnswer:@"Yes"];
+                                }];
     
-    UIAlertAction *noAction = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"What a bummer!" message:@"Neither are you." preferredStyle:UIAlertControllerStyleAlert];
-        
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Okay :(" style:UIAlertActionStyleCancel handler:nil];
-        [alert addAction:cancelAction];
-        [self presentViewController:alert animated:YES completion:nil];
-        
-    }];
+    UIAlertAction *noAction = [UIAlertAction
+                               actionWithTitle:@"No"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {
+                                   [self checkAnswer:@"No"];
+                               }];
     
     [alert addAction:yesAction];
     [alert addAction:noAction];
     [self presentViewController:alert animated:YES completion:nil];
 }
-
 
 @end
